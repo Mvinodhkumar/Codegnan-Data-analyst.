b68e3f7062c9fc48c9df18e80bf32be5ff48cc14
Mvinodhkumar <vinodhkumarmuppidi@gmail.com>
2026-04-16 10:45:53 +0530

    Today we discuss abount keyword,comments,
    boolen type
    tokens

---
+++ day2.py
@@ -0,0 +1,70 @@
+'''
+a=9
+print(a)
+b=10
+print(b)
+_c=11
+print(_c)
+
+we should not use space in special characters
+num=90
+note-we are going use meaningfull words or names for defining variables
+-------------------------------------------------------------------------
+keywords
+------
+-->this keywords not gonna use as a variable
+if,ifelse,while
+'''
+'''
+a,b,c=10,20,30
+print(a)
+print(b)
+print(c)
+
+a,b,c=10,20,30
+print(f"\n{a}\f\n{b}\f{c}")
+--------------------------------------------------------------------------
+Tokens
+----------
+-->nothing but a small program or code  in python to complete the task
+
+a, b, c=5, 6, 7
+print(a+b)
+----------------------------------------------------------------------------
+comments
+---------
+comments are two types
+1)single line
+-->this is used to explain abount that particular line in the code,for this we can use#simble
+x,y,z=10,20,30 #hereb in the same line, i have assign 3 variable and value
+print(x+y)
+2)multi-line
+-->to comment more than single line we can use multi line commenting we can use
+""" """or''' '''
+------------------------------------------------------------------------------
+boolen type
+-------------
+'''
+'''
+a=20
+b=10
+print(a==b)
+'''
+num=7
+if num%2==0:
+   print(f"{num} is even number")
+else:
+   print(f"{num} is odd number")
+
+
+
+
+
+
+
+
+
+
+
+
+
