82441139c816a148fe1c0c5b98116eeb532e8556
Mvinodhkumar <vinodhkumarmuppidi@gmail.com>
2026-04-17 10:58:00 +0530

    day-3 operators
    arithmetic operator
    assignment operator
    comparision operator
    logical operator
    identity opertor
    member operator
    bitwise operator

---
+++ day3.py
@@ -0,0 +1,139 @@
+'''
+opertores
+---------
+1.Arithmetic operator
+2.Assignment operator
+3.Comparision operator
+4.Logical operator
+5.Identity opertor
+6.Member operator
+7.Bitwise operator
+
+Arithmetic operator-->>+,-,%,*,**
+ex
+num=10
+num2=20
+num3=30
+print(num+num2+num3)
+=60
+
+num=6
+num2=10
+print(num % num2)
+=6
+
+digi=10
+digi2=9
+print(digi * digi2)
+=90
+
+digi=90.9
+digi2=5.5
+print(digi//digi2)
+=16.0
+
+Assignment operator-->=,+=,-=,%=,//=,**=
+------------------------------------------
+-->(=)to assign datatype to a variable
+num=10
+num +=9
+print(num)
+=19
+num=20
+num -=12
+print(num)
+=8
+num=9
+num %=2
+print(num)
+=1
+
+Comparision operator-->==,!=,>,<,>=,<=
+-----------------------------------------
+x=10
+y=40
+print(10!=40)
+
+x=10
+y=9
+print(x>y)
+=true
+num=20
+num=10
+print(num>=num)
+=true
+
+Logical operator-->and,or
+----------------------------------------
+and-->the 2 condition should be true
+v=15
+n=22
+print(v<10 and n>20)
+false
+num=10
+num=20
+print(num>5 or num<20)
+true
+
+Identity operator-->is,isnot
+-------------------------------------
+x=9
+y=9
+print(x is y)
+list=[1,2]
+list2 =[1,2]
+list3 =list
+print(id(list))
+print(id(list2))
+print(id(list is list3))
+true
+(is not)-->this operator is used to check the object
+list=[2,4]
+list2=[2,5]
+print(list is not list2)
+true
+
+Member operator-->in,notin
+------------------------
+(in)-->is used to check, it present init
+string=[9,1]
+print(1 in string)
+true
+
+Bitwise operator-->&,/,^,~,<<,>>
+-------------------------------------
+&-->Bitwise and
+print(4&4)'''
+print(9^9)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
