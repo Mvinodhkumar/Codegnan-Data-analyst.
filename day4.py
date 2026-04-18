5769f98099aeb34dd477600fa9154fe2737f3b8c
Mvinodhkumar <vinodhkumarmuppidi@gmail.com>
2026-04-18 11:01:33 +0530

    Today i learn mutable,  immutable,
    Integers
    String
    replace
    spilt
    strip
    join
    upper
    lower

---
+++ day4.py
@@ -0,0 +1,85 @@
+'''
+mutable data type
+--------------------
+-->I can modify directly on the variable, which the data type have taken
+eg-list,dict
+immutable
+--------------------
+-->Where can not be modifield directly on the variable assign to the data type
+eg-int,string
+1)integer or int
+--------------------------------------------------------
+-->storing number or digit in the variable is called int
+num=90
+float
+-->storing decimal value in the variable is called float
+num=90.22
+
+-->indexing
+--------------------------------------------------------
+-->this is used to get the particular substra
+any = "Python is a programming language"
+print(any[9])
+syntax-variable_name [index_position]
+
+concatenation
+--------------------------------------------------------
+-->A+act as different way,if we are adding 2 integers it act like adding
+in other cases such as list,l string and tuple it act like concatenation
+eg-
+num_1=20
+num_2=10
+print(num1+num2)
+'''
+'''
+2)string or str
+--------------------------------------------------------
+
+string is collect of char that are inclose in '',"",
+-->It is immutable data type
+methods
+---------------------------------------------------------
+replace()
+-->this method is used to replace or change old string with new string
+syntax
+----------------------------------------------------------
+variable _name.replace("old string","new string")
+any=("Python is programming language")
+print=(any.replace("Python","Java"))
+
+split()
+---------------------------------------------------------
+-->this method is used separate the string using a substring and it will spilt into
+list such as before the substring is one index and after the substring is another
+index.
+syntax-->variable_name.spilt("substring")
+name="allu arjun"
+print(name.split(" "))
+
+strip()
+-----------------------------------------------------------
+-->this method is used to removed from 1st index position
+num="ram charan"
+print(name.strip(" ")
+
+
+
+join ()
+syntax
+--------------------------------------------------------
+variable_name.join("variable name")
+
+lower()
+upper()
+'''
+'''
+'''
+num=int(input())
+print(f"{num} is a even")
+
+
+
+
+
+
+
