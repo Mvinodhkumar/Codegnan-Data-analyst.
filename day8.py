3bcaf56477342c86531804b154b98caa318b6674
Mvinodhkumar <vinodhkumarmuppidi@gmail.com>
2026-04-30 10:34:52 +0530

    Day8-python learning on user input and if else statment

---
+++ day8.py
@@ -0,0 +1,98 @@
+'''
+User input
+----------
+any = int (input(" enter a number : "))
+print (type(any))
+
+string data
+--------------
+an = input("enter the word: ")
+print(type(an)
+
+a,b = map(int,input("enter two numbers:").split())
+print(a)
+print(type(a))
+print(b)
+print(type(b))
+
+list data type
+-------------- 
+cv = list(map(int,input("enter the number:").split()))
+print (type(cv))
+print (cv)
+
+tuple data type
+---------------- 
+cv = tuple(map(int,input("enter the number:").split()))
+print (type(cv))
+print (cv)
+
+formating string
+----------------- 
+v=9
+i=10
+print(f"v + i ={v+i}")
+
+if statement
+---------------
+this is used to check condition is true or not 
+an=9
+if an==9:
+ print (f"an is equal to {9}")
+
+else statement
+----------------
+-->else is fall-back statement, incase if statement
+become false,it will enter into else
+an=9
+if an > 9:
+ print (f"an is equal to {9}")
+else:
+ print (f"an is not equal to {9}")
+
+
+a=9
+b=10
+if a>b :
+ print(f" a is greater {a}")
+else:
+ print(f" b is greater {b}")
+
+v= eval(input("enter the number: "))
+print(type(v))
+print(v)
+
+age= int(input("enter the age:"))
+if age>=18:
+    print("your age is eligible to vote")
+else:
+    print(f"you have to wait{age-18} to vote")'''
+
+marks = int(input("enter your marks:"))
+if marks<35:
+    print("failed")
+else:
+    print(f"pass")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+ 
+
+                    
