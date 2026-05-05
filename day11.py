aee81d861b57947bd30b4cf2f16c19a976894a10
Mvinodhkumar <vinodhkumarmuppidi@gmail.com>
2026-05-05 20:03:58 +0530

    Day-11 pythyon

---
+++ day11.py
@@ -0,0 +1,19 @@
+'''
+any = eval(input("Enter items of list:"))
+empty_ = []
+for i in any:
+    if i not in empty_:
+        empty_.append(i)
+print(empty_)
+'''
+
+nums = [10,20,2,76,4,45,99]
+max1 = 0
+max2 = 0
+for num in nums:
+    if num > max1
+    max2 = max1
+    max1 = num
+printf(f"{max} is the max number in the list {num}")
+
+
