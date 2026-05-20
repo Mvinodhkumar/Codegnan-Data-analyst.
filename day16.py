d428fb3cdf5e270e44c725679f0dce9e8051bd36
Mvinodhkumar <vinodhkumarmuppidi@gmail.com>
2026-05-20 19:41:29 +0530

    Day-16 Modules

---
+++ day16.py
@@ -0,0 +1,70 @@
+'''
+Modules
+--------
+-->A module is a file containing python code such as:
+-variables
+-function
+-classes
+
+import coding
+print(coding.sub(9,2))
+print(coding.div(9,9))
+print(coding.multi(9,9))
+
+Types of modules
+----------------
+-->In-built modules
+-->user define
+-os
+-math
+-sys 
+
+-->To use all this module, we have to import
+with module name
+
+ways to import modules
+----------------------
+1.using alias name
+2.import entire module
+3.import all functions
+from math import
+print(pow(9,8))
+4.import specific function
+
+math module
+------------'''
+import math
+print(math.sqrt (49)))
+print(math.sin(1/2))
+print(math.factorial(5))
+
+sys module
+--------------
+import says
+print(sys.version)
+print(sys.path)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
